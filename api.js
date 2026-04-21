eb41aa7ed996abccc69c0488f719adec881331e5
dasuttam05-netizen <das.uttam05@gmail.com>
2026-04-21 20:56:15 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((config) => {
